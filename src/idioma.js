aa2c65053ac2388a769972fbaf49cdb0cd01144c
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-07 10:42:12 +0200

    Clase modificada, closes #4

--- src/idioma.js
+++ src/idioma.js
@@ -5,13 +5,11 @@ class Idioma{
     //vamos a crear un array asociativo de strings para relacionar una palabra con su traducción y significado
 
 
-      constructor(){
-        var descripcion = new Array();
-        var listaVocab = new Array();
+      constructor(descripcion, listaVocab){
+        descripcion = new Array();
+        listaVocab = new Array();
         this.listaVocab = ["Formato Palabra "]
         this.descripcion = [" Significado "]
-
-
       }
 
 
@@ -19,21 +17,10 @@ class Idioma{
        //hacemos un bucle en el que recorremos el array
 
      MostrarVocab(){
-       for(var i in this.listaVocab){
-         console.log(this.listaVocab[i]+":" + this.descripcion[i]);
-
-        }
     }
 //
  //FUNCIÓN PARA AÑADIR
     AniadirVocab(palabra, significado){
-
-        this.descripcion.push(significado);
-        this.listaVocab.push(palabra);
-
-      //añadimos una palabra de ejemplo
-
-
     }
 
 
@@ -44,12 +31,5 @@ class Idioma{
     //función que muestra 1 dato curioso sobre dicha lengua.
 }
 
-var diccionario = new Idioma();
-palabra = "ordenador";
-significado = "Ordinateur. Sinónimo de computador o computadora y se refiere a la máquina electrónica capaz de almacenar información y tratarla automáticamente";
 
-diccionario.AniadirVocab(palabra,significado);
-diccionario.MostrarVocab();
-
-
-module.exports = Idioma;
+//module.exports = Idioma;
