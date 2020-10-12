0225836e1fe9809c52c50203b36669f42f21d620
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-13 00:13:33 +0200

    Modificando gruntfile.js #15

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,18 @@
+
 'use strict';
 
 module.exports = function (grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jest: {
+      options: {
+        coverage: true,
+        testPathPattern: /.*-test.js/
+      }
+    }
+});
+
+  grunt.loadNpmTasks('grunt-jest');
+  grunt.registerTask('test', ['jest']);
 
 };
