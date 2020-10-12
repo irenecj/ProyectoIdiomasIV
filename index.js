26382ab2dca717a5d9cc9d58b5fc2f2a312c861f
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-13 00:16:02 +0200

    Añadiendo llamada a MostrarPalabra #16

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Idioma = require("../src/idioma.js");
+const Idioma = require("./src/idioma.js");
 
-var diccionario = new Idioma();
+var idioma = new Idioma();
 //leemos los datos que meta el usuario
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -10,33 +10,40 @@ const rl = readline.createInterface({
 
 
 palabra = "ordenador";
-significado = "Ordinateur. Sinónimo de computador o computadora y se refiere a la máquina electrónica capaz de almacenar información y tratarla automáticamente";
-diccionario.AniadirVocab(palabra,significado);
+significado = "Ordinateur. Sinónimo de computador o computadora. Se refiere a la máquina electrónica capaz de almacenar información y tratarla automáticamente";
+idioma.AniadirVocab(palabra,significado);
 palabra = "libro";
 significado = "livre";
-diccionario.AniadirVocab(palabra, significado);
+idioma.AniadirVocab(palabra, significado);
 
 var readlineRecursivo = function() {
-  rl.question("¿Qué desea hacer? Pulse: \n 1 para añadir vocabulario. \n 2 para mostrar el vocabulario existente. \n 3 para salir. \n", function(tecla){
+  rl.question("¿Qué desea hacer? Pulse: \n 1 para añadir vocabulario. \n 2 para mostrar el vocabulario existente. \n 3 para buscar una palabra concreta. \n 4 para salir. \n", function(tecla){
     switch(tecla){
       case "1":
       rl.question("¿Qué palabra has aprendido? ", function(palabra){
         rl.question("¿Cuál es el significado o traducción de la palabra? ", function(significado){
-          diccionario.AniadirVocab(`${palabra}`,`${significado}`);
+          idioma.AniadirVocab(`${palabra}`,`${significado}`);
           readlineRecursivo();
         });
       });
       break;
 
-
       case "2":
-      diccionario.MostrarVocab();
+      idioma.MostrarVocab();
       readlineRecursivo();
       break;
 
       case "3":
+      rl.question("¿Qué palabra desea buscar?", function(palabra){
+        idioma.MostrarPalabra(`${palabra}`);
+        readlineRecursivo();
+      });
+      break;
+
+      case "4":
       rl.close();
       break;
+
     }
   });
 };
