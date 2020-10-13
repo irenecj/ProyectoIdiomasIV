498da5dfc7720fa9e3d955c4366ba5daff796f14
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-13 13:12:18 +0200

    Añado clase para gestionar cuando una palabra no es de tipo String #19

---
+++ src/excepciones/NoString.js
@@ -0,0 +1,12 @@
+//clase representar un error cuando la palabra no es un STRING
+class NoString extends Error { //clase error ya viene en javascript
+
+	constructor(mensajeError) {
+		super(mensajeError);
+		this.name = 'NoString';
+		this.message = mensajeError;
+	}
+
+}
+
+module.exports = NoString;
