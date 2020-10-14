d759f502205863bb69c4653f24d392c609941154
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-14 10:12:37 +0200

    Implementando función para mostrar palabras que empiezan por una determinada letra #20

--- src/idioma.js
+++ src/idioma.js
@@ -1,13 +1,14 @@
 /* Clase principal que contiene el vocabulario, las frases típicas y datos curiosos sobre el idioma */
 
-
+const NoString = require("./excepciones/NoString");
+const NoEncontrada = require("./excepciones/NoEncontrada");
 class Idioma{
 
       constructor(descripcion, listaVocab){
         descripcion = new Array();
         listaVocab = new Array();
-        this.listaVocab = ["Formato a mostrar -->  Palabra "]
-        this.descripcion = [" Significado "]
+        this.listaVocab = ["Formato a mostrar -->  Palabra "];
+        this.descripcion = [" Significado "];
       }
 
       //FUNCIÓN PARA COMPROBAR QUE UNA PALABRA ES UN STRING
@@ -15,24 +16,23 @@ class Idioma{
         const numeros = /^[0-9]*$/;
         const palabraNum = numeros.test(palabra);
         if(palabraNum == true){
-          throw new Error('La palabra debe ser de tipo "string"');
+          throw new NoString('La palabra debe ser de tipo "string"');
         }else{
           return palabraNum;
         }
       }
 
+      //FUNCIÓN PARA AÑADIR UNA EXPRESIÓN
+
       //FUNCIÓN PARA AÑADIR UNA PALABRA AL VOCABULARIO
       AniadirVocab(palabra, significado){
-        var palabraIsString = this.ComprobarString(palabra);
-        var significadoIsString = this.ComprobarString(significado);
-        if( palabraIsString == false && significadoIsString == false){
-          try{
+        var palabraNoString = this.ComprobarString(palabra);
+        var significadoNoString = this.ComprobarString(significado);
+        if( palabraNoString == false && significadoNoString == false){
             this.descripcion.push(significado);
             this.listaVocab.push(palabra);
-          } catch (exception) {
-			         throw exception;
-          }
         }
+
       }
 
       //FUNCIÓN PARA MOSTRAR TODAS LAS PALABRAS DEL VOCABULARIO
@@ -46,8 +46,8 @@ class Idioma{
       MostrarPalabra(palabra){
         var encontrada = 0;
         var indice;
-        var esString = this.ComprobarString(palabra);
-        if(esString == false){
+        var noString = this.ComprobarString(palabra);
+        if(noString == false){
           for(var i in this.listaVocab){
             if(palabra == this.listaVocab[i]){
               encontrada++;
@@ -59,15 +59,16 @@ class Idioma{
         if(encontrada > 0){
           console.log(this.listaVocab[indice]+" : " + this.descripcion[indice]);
         }else{
-          console.log("La palabra que busca aún no está añadida");
+          throw new NoEncontrada("La palabra que busca aún no está añadida");
         }
+
       }
 
       CambiarDescripcion(palabra, descripcionNueva){
         var encontrada = 0;
         var indice;
-        var esString = this.ComprobarString(palabra);
-        if(esString == false){
+        var noString = this.ComprobarString(palabra);
+        if(noString == false){
           for(var i in this.listaVocab){
             if(palabra == this.listaVocab[i]){
               var descripcionActual = this.descripcion[i];
@@ -80,7 +81,19 @@ class Idioma{
       if(encontrada > 0){
         this.descripcion[indice] = descripcionNueva;
       }else{
-        console.log("La palabra que busca no se ha encontrado.");
+        throw new NoEncontrada("La palabra que busca no se ha encontrado");
+      }
+    }
+
+    //MOSTRAR LISTADO DE PALABRAS QUE COMIENZAN POR UNA LETRA
+    ClasificaLetra(letra){
+      for(var i in this.listaVocab){
+        var comienza = this.listaVocab[i].startsWith(`${letra}`);
+        if(comienza == true){
+          console.log(this.listaVocab[i]+" : " + this.descripcion[i]);
+        }else{
+          throw new NoEncontrada("No hay ninguna palabra que comience por esa letra");
+        }
       }
     }
 
