1a813981c630c6b8e6df72c35ec223774e2ee958
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-16 00:05:42 +0200

    Testeando función para ordenar alfabéticamente #25

--- tests/idioma.test.js
+++ tests/idioma.test.js
@@ -2,72 +2,67 @@ const Idioma = require("../src/idioma.js");
 const NoString = require("../src/excepciones/NoString.js");
 
 //VARIABLES A UTILIZAR
-var palabra;
-var significado;
-var tam_vocab;
-var tam_descrip;
+var palabra, significado;
+var tam_vocab, tam_descrip;
 var thrown_error;
 var expectedError;
 var letra;
 var encontrada;
 var expectedEncontrada;
-var expresion;
-var explicacion;
-const idioma = new Idioma("Hola", "Salut");
+var expresion, explicacion;
+var resultado;
+const idioma = new Idioma("Ordenador", "Ordinateur.Sinónimo de computador o computadora. Se refiere a la máquina electrónica capaz de almacenar información y tratarla automáticamente");
 
 describe("Testeando la clase idioma.js", () => {
 
   describe("Testeando el constructor", () =>{
     test("Comprobando que funciona correctamente", () => {
-      expect(idioma.listaVocab[0]).toBe("Hola");
-      expect(idioma.descripcion[0]).toBe("Salut");
+      expect(idioma.listaVocab[0]).toBe("Ordenador");
+      expect(idioma.descripcion[0]).toBe("Ordinateur.Sinónimo de computador o computadora. Se refiere a la máquina electrónica capaz de almacenar información y tratarla automáticamente");
     });
   });
-  describe("Testeando el método CompruebaString()", () => {
+  describe("Testeando el método compruebaString()", () => {
     test("Comprobando que lanza error si la palabra no es un string", () => {
       palabra = "1";
-      thrown_error = () => idioma.ComprobarString(palabra);
+      thrown_error = () => idioma.comprobarString(palabra);
       expectedError = new NoString('La palabra debe ser de tipo "string"');
 
       expect(thrown_error).toThrow(expectedError);
     });
   });
 
-  describe("Testeando el método AniadirVocab()", () => {
+  describe("Testeando el método aniadirVocab()", () => {
    test("Comprobando que no se lanza el error al pasarle un 'string'", () => {
       palabra = "Silla";
-      significado = "Chaise";
-      thrown_error = () => idioma.AniadirVocab(palabra, significado);
+      significado = "Chaise.Asiento individual con patas y respaldo.";
+      thrown_error = () => idioma.aniadirVocab(palabra, significado);
       expectedError = new NoString('La palabra debe ser de tipo "string"');
 
       expect(thrown_error).not.toThrow(expectedError);
     });
 
-   test("Comprobando que se inserta la palabra correctamente", () => {
+   test("Comprobando que se insertan la palabra y la descripción correctamente", () => {
      //al insertar la palabra el tamaño del vector 'listaVocab' debe incrementar en 1
       var tam_vocab_inicial = idioma.listaVocab.length;
-      palabra="Mesa";
-      idioma.listaVocab.push(palabra);
-      tam_vocab = idioma.listaVocab.length;
-
-      expect(tam_vocab).toBe(tam_vocab_inicial+1);
-
-    });
-   test("Comprobando que se inserta la descripción correctamente", () => {
-     //al insertar el significado el tamaño del vector 'descripcion' debe incrementar en 1
       var tam_descrip_inicial = idioma.descripcion.length;
-      descripcion = "Table";
-      idioma.descripcion.push(descripcion);
+      palabra="Mesa";
+      descripcion = "Table.Mueble formado por un tablero horizontal, sostenido por uno o varios pies, con la altura conveniente para poder realizar alguna actividad sobre ella o dejar cosas encima.";
+
+      idioma.aniadirVocab(palabra, descripcion);
+
+      tam_vocab = idioma.listaVocab.length;
       tam_descrip = idioma.descripcion.length;
 
+      expect(tam_vocab).toBe(tam_vocab_inicial+1);
       expect(tam_descrip).toBe(tam_descrip_inicial+1);
+
     });
     test("Comprobando que la palabra y su significando se añaden correctamente", () => {
       //el último elemento de cada vector, debe ser la palabra y el significado que acabamos de añadir
       palabra = "Informática";
-      significado = "Informatique.";
+      significado = "Informatique.Conjunto de conocimientos técnicos que se ocupan del tratamiento automático de la información por medio de computadoras.";
 
-      idioma.AniadirVocab(palabra, significado);
+      idioma.aniadirVocab(palabra, significado);
 
       tam_vocab = idioma.listaVocab.length;
       tam_descrip = idioma.descripcion.length;
@@ -75,11 +70,11 @@ describe("Testeando la clase idioma.js", () => {
       var ultimoDescrip = idioma.descripcion[tam_descrip-1];
 
       expect(ultimoLista).toBe("Informática");
-      expect(ultimoDescrip).toBe("Informatique.");
+      expect(ultimoDescrip).toBe("Informatique.Conjunto de conocimientos técnicos que se ocupan del tratamiento automático de la información por medio de computadoras.");
     });
   });
 
-  describe("Testeando el método MostrarVocab()", () => {
+  describe("Testeando el método mostrarVocab()", () => {
     test("Comprobamos que los dos vectores son iguales", () => {
       //para que cada palabra coincida con su significado, los dos vectores deben tener el mismo tamaño
       tam_vocab = idioma.listaVocab.length;
@@ -90,13 +85,13 @@ describe("Testeando la clase idioma.js", () => {
 
     test("Comprobamos que se han mostrado TODAS las palabras", () => {
       //el vector que mostramos debe ser igual que el vector de palabras, sino no se habrán mostrado todas
-      var tam_vector_esperado = idioma.MostrarVocab().length;
+      var tam_vector_esperado = idioma.mostrarVocab().length;
       var tam_vocab = idioma.listaVocab.length;
       expect(tam_vector_esperado).toEqual(tam_vocab);
     });
   });
 
-  describe("Testeando el método MostrarPalabra()", () => {
+  describe("Testeando el método mostrarPalabra()", () => {
     //para que la palabra coincida con su significado, los dos vectores deben tener el mismo tamaño
     test("Comprobamos que los dos vectores son iguales", () => {
       tam_vocab = idioma.listaVocab.length;
@@ -107,7 +102,7 @@ describe("Testeando la clase idioma.js", () => {
      test("Comprobando que notifica correctamente que la palabra no se encuentra en el listado de vocabulario", () => {
        //mostramos al usuario un mensaje que indica que la palabra no existe, permitiéndole buscar otra o realizar otra acción como añadir esa palabra
        palabra = "Gominola";
-       encontrada = idioma.MostrarPalabra(palabra);
+       encontrada = idioma.mostrarPalabra(palabra);
        expectedEncontrada = "La palabra que busca no se ha encontrado";
 
        expect(encontrada).toBe(expectedEncontrada);
@@ -115,7 +110,7 @@ describe("Testeando la clase idioma.js", () => {
      test("Comprobando que funciona correctamente al pasarle una palabra existente en la lista de vocabulario", () => {
        //nos devuelve la palabra con su significado
        palabra = "Mesa";
-       var resultado = idioma.MostrarPalabra(palabra);
+       resultado = idioma.mostrarPalabra(palabra);
        var indicePalabra = idioma.listaVocab.indexOf(palabra);
        significado = idioma.descripcion[indicePalabra];
 
@@ -124,7 +119,7 @@ describe("Testeando la clase idioma.js", () => {
      test("Comprobando que la palabra se corresponde con la descripción", () => {
        //comprobamos que el significado es el de dicha palabra y no otro
        palabra = "Mesa";
-       significado = "Table";
+       significado = "Table.Mueble formado por un tablero horizontal, sostenido por uno o varios pies, con la altura conveniente para poder realizar alguna actividad sobre ella o dejar cosas encima.";
        var indicePalabra = idioma.listaVocab.indexOf(palabra);
        var indiceDescrip = idioma.descripcion.indexOf(significado);
 
@@ -132,12 +127,12 @@ describe("Testeando la clase idioma.js", () => {
      });
   });
 
-  describe("Testeando el método CambiarSignificado()", () =>{
+  describe("Testeando el método cambiarSignificado()", () =>{
     test("Comprobando que notifica correctamente que la palabra no se encuentra en el listado de vocabulario", () => {
       //si la palabra no se encuentra notificamos al usuario para que así decida si realizar otra acción, como añadir dicha palabra
        palabra = "Piano";
        significado = "Piano. Instrumento musical.";
-       encontrada = idioma.CambiarSignificado(palabra,significado);
+       encontrada = idioma.cambiarSignificado(palabra,significado);
        expectedEncontrada = "La palabra que busca no se ha encontrado";
 
        expect(encontrada).toBe(expectedEncontrada);
@@ -146,18 +141,18 @@ describe("Testeando la clase idioma.js", () => {
        //vemos que en el array 'descripcion' ahora el significado correspondiente a la palabra es el nuevo
        palabra = "Mesa";
        significadoNuevo = "Cambio descripción de la mesa";
-       idioma.CambiarSignificado(palabra, significadoNuevo);
+       idioma.cambiarSignificado(palabra, significadoNuevo);
        var indicePalabra = idioma.listaVocab.indexOf(palabra);
        significado = idioma.descripcion[indicePalabra];
        expect(significado).toBe(significadoNuevo);
      });
   });
 
-  describe("Testeando el método ClasificaLetra()", () => {
+  describe("Testeando el método clasificaLetra()", () => {
     test("Comprobando que notifica correctamente si no hay ninguna palabra que comience por dicha letra", () => {
       //si no hay palabras que empiecen por dicha letra se notifica al usuario
       letra = "W";
-      encontrada = idioma.ClasificaLetra(letra);
+      encontrada = idioma.clasificaLetra(letra);
       expectedEncontrada = "La palabra que busca no se ha encontrado";
 
       expect(encontrada).toBe(expectedEncontrada);
@@ -166,7 +161,7 @@ describe("Testeando la clase idioma.js", () => {
     test("Comprobando que muestra el listado de palabras correctamente si la letra es mayúscula", () => {
       //debe ser indiferente si le pasamos una letra mayúscula o minúscula.
       letra = "M";
-      var resultado = idioma.ClasificaLetra(letra);
+      resultado = idioma.clasificaLetra(letra);
 
       expect(resultado.length).toBe(1);
     });
@@ -174,18 +169,43 @@ describe("Testeando la clase idioma.js", () => {
     test("Comprobando que muestra el listado de palabras correctamente si la letra es minúscula", () => {
       //debe ser indiferente si le pasamos una letra mayúscula o minúscula.
       letra = "m";
-      var resultado = idioma.ClasificaLetra(letra);
+      resultado = idioma.clasificaLetra(letra);
 
       expect(resultado.length).toBe(1);
     });
   });
 
-  describe("Testeando el método AniadirExpresiones()", () => {
+  describe("Testeando el método ordenarAlfabeto()", () => {
+    test("Comprobando que notifica correctamente si no se introduce el orden debido", () => {
+      resultado = idioma.ordenarAlfabeto("OrdenNoValido");
+      var resultadoEsperado = "Debe introducir orden 'ascendente' o 'descendente'";
+
+      expect(resultado).toBe(resultadoEsperado);
+    });
+    test("Comprobando que ordena ascendentemente de forma correcta", () => {
+      var idio_nuevo = new Idioma("Vino.","Vin.");
+      idio_nuevo.aniadirVocab("Bonito.", "Jolie.");
+      idio_nuevo.ordenarAlfabeto("Ascendente");
+
+      expect(idio_nuevo.listaVocab[0]).toBe("Bonito.");
+      expect(idio_nuevo.listaVocab[1]).toBe("Vino.");
+
+    });
+    test("Comprobando que ordena descendentemente de forma correcta", () => {
+      var idio_nuevo = new Idioma("Vino", "Vin");
+      idio_nuevo.aniadirVocab("Bonito.", "Jolie.");
+      idio_nuevo.ordenarAlfabeto("Descendente");
+
+      expect(idio_nuevo.listaVocab[0]).toBe("Vino");
+      expect(idio_nuevo.listaVocab[1]).toBe("Bonito.");
+    });
+  });
+  describe("Testeando el método aniadirExpresiones()", () => {
     test("Comprobando que se incrementa el tamaño del vector al añadir", () => {
       expresion = "Montar un pollo";
       explicacion = "Te has venido arriba. Has montado una bronca sin venir a cuento. Un escándalo."
       var tamañoOriginal = idioma.expresiones.length;
-      idioma.AniadirExpresiones(expresion, explicacion);
+      idioma.aniadirExpresiones(expresion, explicacion);
       var tamañoActual = idioma.expresiones.length;
 
       expect(tamañoActual).toEqual(tamañoOriginal + 1);
@@ -194,7 +214,7 @@ describe("Testeando la clase idioma.js", () => {
       expresion = "Hablar por los codos.";
       significado = "Cuando una persona habla mucho o está hablando en todo momento, no se calla nunca.";
 
-      idioma.AniadirExpresiones(expresion, significado);
+      idioma.aniadirExpresiones(expresion, significado);
 
       var tamaño = idioma.expresiones.length;
       var ultimo = idioma.expresiones[tamaño - 1];
@@ -207,11 +227,14 @@ describe("Testeando la clase idioma.js", () => {
       expect(ultimo).toBe(expresionEsperada);
     });
   });
-
-  test("Comprobando el método MostrarExpresiones()", () => {
-    var tam_vector_expresiones = idioma.MostrarVocab().length;
-    var tam_vocab = idioma.listaVocab.length;
-    expect(tam_vector_expresiones).toEqual(tam_vocab);
+  describe("Testeando el método mostrarExpresiones()", () => {
+    test("Comprobando que se muestran TODAS las expresiones", () => {
+      //el tamaño del vector devuelto tiene que ser el mismo que el del vector de expresiones
+      var tam_vector_expresiones = idioma.mostrarVocab().length;
+      var tam_vocab = idioma.listaVocab.length;
+      expect(tam_vector_expresiones).toEqual(tam_vocab);
+    });
   });
+
 
 });
