1f6b8115e3e2bc4dbc7ae3c035ab7efb3e2fd05f
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-15 19:12:54 +0200

    Clase con error NoString finalizada, closes #19

--- src/excepciones/NoString.js
+++ src/excepciones/NoString.js
@@ -1,4 +1,4 @@
-//clase representar un error cuando la palabra no es un STRING
+//clase para representar un error cuando la palabra no es un STRING
 class NoString extends Error { //clase error ya viene en javascript
 
 	constructor(mensajeError) {
