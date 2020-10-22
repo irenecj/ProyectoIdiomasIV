65862704b8545410c825938d02d216115ce93dbe
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-10-22 18:07:09 +0200

    Añadiendo nueva excepción #26

---
+++ src/excepciones/NoEncontrada.js
@@ -0,0 +1,12 @@
+//clase para representar un error cuando no se encuentra ninguna palabra
+class NoEncontrada extends Error { //clase error ya viene en javascript
+
+	constructor(mensajeError) {
+		super(mensajeError);
+		this.name = 'NoEncontrada';
+		this.message = mensajeError;
+	}
+
+}
+
+module.exports = NoEncontrada;
