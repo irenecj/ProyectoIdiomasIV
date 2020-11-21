28a2a8f6ed36f8fd24341131f93d8dfcd14e31ff
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-11-21 14:05:00 +0100

    Edito fichero hello.js [skip ci] #45

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
   const { name = 'World' } = req.query
 
-  res.status(200).send(`Helloo ${name}! Prueba realizada para el proyecto de Irene Cano`)
+  res.status(200).send(`Prueba realizada para el proyecto de Irene Cano ${name}!`)
 }
