dd4e632bad7cc0513e54d3e1c3c7a820d2d85e6e
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-11-25 00:08:12 +0100

    Implemento función para buscar una palabra concreta, correspondiente a la HU3 [skip ci] #17 #47

---
+++ functions/listado.js
@@ -0,0 +1,43 @@
+const data = require("./data/data.json")
+const Idioma = require("../src/idioma.js")
+
+exports.handler = async event => {
+  var palRecibida = event.queryStringParameters.palRecibida
+  var idioma = new Idioma("español","frances");
+  var palabra;
+  var significado;
+  var datos=[];
+
+  for(var i = 0; i < data.traducciones.length; i++){
+
+        palabra = data.traducciones[i].palabra;
+        significado = data.traducciones[i].significado;
+        idioma.aniadirVocab(palabra,significado)
+  }
+
+  var resultado = palRecibida;
+  var formatoValido = idioma.comprobarFormato(palRecibida);
+
+  if(formatoValido == true){
+    var pos = -1
+    for(var j  = 0 ; j < idioma.listado.length ; j++){
+      if(palRecibida == idioma.listado[j].getPalabra()){
+        pos = j;
+      }
+    }
+    if(pos == -1){
+      resultado = "Esa palabra no está en nuestro diccionario ";
+    }
+    else{
+      resultado = idioma.listado[pos].getTraduccion();
+    }
+
+  }else{
+    resultado = "La palabra debe introducirse en mayúsculas y debe acabar con un punto final.";
+  }
+
+  return{
+    statusCode:200,
+    body: resultado.toString()
+  }
+}
