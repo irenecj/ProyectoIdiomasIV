1bdc1d9a4e4cc7ba78ac9e1978cb0192709467d8
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-11-25 01:24:28 +0100

    Añado comando para mostrar buscar una palabra concreta [skip ci] #17 #48

--- functions/bot.js
+++ functions/bot.js
@@ -22,6 +22,33 @@ function listadoVocab(){
   return resultado;
 }
 
+function palabraConcreta(pal){
+  var palabraEncontrada;
+  var significado;
+  var resultado=" ";
+  var pos = -1;
+  var palValida = formato(pal);
+  if(palValida){
+    for(var i = 0; i < data.traducciones.length; i++){
+      if(pal == data.traducciones[i].palabra){
+        pos = i;
+      }
+    }
 
+    if(pos == -1){
+      resultado = "La palabra que busca no se encuentra registrada"
+    }
+    else{
+      palabraEncontrada = pal;
+      significado = data.traducciones[pos].significado;
+      resultado = palabraEncontrada;
+      resultado = palabraEncontrada.concat('->',significado);
+    }
+  }else{
+    resultado = "El formato introducido no es correcto: la palabra debe introducirse en mayúsculas y acabando en punto final."
+  }
 
-module.exports = { formato, listadoVocab };
+  return resultado;
+}
+
+module.exports = { formato, listadoVocab, palabraConcreta };
