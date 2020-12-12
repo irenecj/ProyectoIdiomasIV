9836490e5868510c9df26a9fd9755a717b66c334
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-12-12 13:11:23 +0100

    Testeando constructor y método para añadir traducción #7 en clase controladora [skip ci] #54

--- tests/controller.test.js
+++ tests/controller.test.js
@@ -1,7 +1,12 @@
 const Controller = require('../src/controller.js');
+const Encontrada = require('../src/excepciones/Encontrada.js');
+const NoEncontrada = require('../src/excepciones/NoEncontrada.js');
+const NoOrden = require('../src/excepciones/NoOrden.js');
 
 const control = new Controller();
 var palabra, significado;
+var thrown_error;
+var expectedError;
 
 describe("Testeando la clase controladora", () => {
   describe("Testeando el constructor", () => {
@@ -21,7 +26,28 @@ describe("Testeando la clase controladora", () => {
 
       expect(tamFinal).toBe(tamInicial+1);
     });
+    test("Comprobando que la palabra se corresponde al significado al añadirse", () => {
+      palabra = "ZAPATO.";
+      significado = "CHAUSSURE.CALZADO QUE CUBRE TOTAL O PARCIALMENTE EL PIE SIN SOBREPASAR EL TOBILLO, CON UNA SUELA DE UN MATERIAL CASI SEIMPRE MÁS DURO QUE EL RESTO.";
 
+      control.nuevaTraduccion(palabra,significado);
+
+      var tam_listado = control.idioma.listado.length;
+      var ultimaPalabra = control.idioma.listado[tam_listado-1].getPalabra();
+      var ultimoSign = control.idioma.listado[tam_listado-1].getSignificado();
+
+      expect(ultimaPalabra).toBe("ZAPATO.");
+      expect(ultimoSign).toBe("CHAUSSURE.CALZADO QUE CUBRE TOTAL O PARCIALMENTE EL PIE SIN SOBREPASAR EL TOBILLO, CON UNA SUELA DE UN MATERIAL CASI SEIMPRE MÁS DURO QUE EL RESTO.");
+    });
+    test("Comprobando que no se pueden añadir palabras ya existentes", () => {
+      palabra = "ESTUCHE.";
+      significado = "TROUSSE.SE USA NORMALMENTE PARA GUARDAR MATERIAL ESCOLAR.";
+
+      thrown_error = () => control.nuevaTraduccion(palabra,significado);
+      expectedError = new Encontrada('La palabra introducida ya existe, por favor registre otra palabra.');
+
+      expect(thrown_error).toThrow(expectedError);
+    });
   });
   describe("Testeando método para mostrar vocabulario (HU1)", () => {
 
